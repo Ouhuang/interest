3328f8cdebb5a6b4a6c054caf3f4c321931d2b33
OuHuang <365908865@qq.com>
2018-05-29 23:48:21 +0800

    rules

--- app/controller/register.js
+++ app/controller/register.js
@@ -2,15 +2,16 @@ const User = require("../service/user")
 
 
 const reg = async (req, res) => {
-    var docs = await User.getUserName(req.body.userName);
+    var docs = await User.getLoginName(req.body.userName);
 
     if (docs !== null)
         return res.json({
-            msg: '用户名重复'
+            msg: '账号重复',
+            type: false
         })
 
     var adduser = await User.addUser(req.body)
-    
+
     res.json({
         data: adduser
     })
--- app/service/user.js
+++ app/service/user.js
@@ -11,6 +11,16 @@ exports.getUserName = (name) => {
         })
     })
 }
+exports.getLoginName = (name) => {
+    return new Promise((res, rej) => {
+        User.find({ login_name: name }).find((err, data) => {
+            if (!err)
+                res(data)
+            else
+                rej(err)
+        })
+    })
+}
 exports.addUser = ({ userName, loginName, passWord, email }) => {
     return new Promise((res, rej) => {
         const user = new User({
