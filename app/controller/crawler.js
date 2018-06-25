1ad99e707189d03e2d0d71ffe091796325e8d1b1
liyanlong <liyanlong@star-v.com.cn>
2018-06-25 18:34:52 +0800

    超级神基因 爬

--- app/controller/crawler.js
+++ app/controller/crawler.js
@@ -6,8 +6,11 @@
 const superagent = require('superagent');
 require('superagent-charset')(superagent)
 const cheerio = require('cheerio');
+const fs = require('fs');
+
 
 const URL = require('url');
+const Path = require('path');
 
 
 let piaotianUrl = 'https://www.piaotian.com/html/8/8410/';
@@ -20,23 +23,32 @@ const crawler = async (req, res) => {
     $('.centent a').each((k, e) => {
         let $e = $(e)
         let val = URL.resolve(piaotianUrl, $e.attr('href'))
-        urlList.push(val)
+        urlList.push({
+            title: $e.text(),
+            url: val
+        })
     })
 
-    let send = await crawlerChild(urlList[0]);
-    res.send(send)
+    let send = await Promise.all(urlList.map(v => superagent.get(v.url).charset('gb2312')));
+
+    let data = send.map((v, k) => crawlerChild(v, urlList[k]));
+
+    var path = Path.resolve(__dirname, '../../public/txt/test.txt');
+
+    fs.writeFile(path, data, 'utf8', (err, fd) => {
+        if (err) throw err;
+        res.send(`<a href="${path} download="超级神基因.txt">点击下载</a>`)
+    })
 }
 
 
 
-const crawlerChild = async (url) => {
-    let { text } = await superagent.get(url).charset('gb2312');
-
-    return text.match(/<br>([\D\d]*?)<\/div>/g);
+const crawlerChild = async (text, title) => {
+    return '\r\n' + title + '\r\n' +
+        text.match(/<br>([\D\d]*?)<\/div>/g).replace(/(<br>)|(<\/div>)/g, '')
+            .replace(/<br \/>/g, '\r\n').replace(/&nbsp;/g, ' ') +
+        '\r\n';
 }
-
-
-
 
 
 
