ec2df8d934cb71ba85088533382fc44bfe585a78
promise <365908865@qq.com>
2018-10-30 18:56:39 +0800

    https

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+'use strict'
 const express = require("express");
 const bodyParser = require('body-parser');
+const os = require('os')
+const fs = require('fs')
+const https = require('https')
 
 const app = express();
 const webRouter = require('./app/router')
@@ -11,5 +15,12 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', webRouter);
 
-app.listen(3000, () =>
-    console.log('启动服务 8080'));
+
+const server = os.platform() === 'linux' ? https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/emm.red/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/emm.red/fullchain.pem')
+}, app) : app
+
+
+server.listen(3000, () =>
+    console.log('启动服务 3000'));
